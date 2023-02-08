e3a3132579701d9dc13003f50df022540dd27e75
rlong711 <you@example.com>
2023-02-08 16:49:47 -0500

    Adding comments to resale shop

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,20 +1,20 @@
 from computer import *
 class ResaleShop:
 
-    # What attributes will it need?
+    #This sets up the attribute inventory that will be used later for buying/selling/refurbishing/etc.
     inventory = list
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
+    #This is a constructor for the attribute needed for this code. 
     def __init__(self):
         self.inventory = []
 
         
 
-    # What methods will you need?
+    #This method sets up the "buying" function of the resale shop by appending information about each computer to the inventory every time a new computer is added. 
     def buy(self, c: Computer): 
         self.inventory.append(c)
    
+   #This method sets up the "selling" function of the resale shop by removing a computer from the inventory list with the condition that the computer has to already be in the inventory.
     def sell(self, c: Computer): 
         if c in self.inventory: 
             self.inventory.remove(c)
